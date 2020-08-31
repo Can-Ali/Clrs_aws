ae273c8bf46cc651530f3fdebb833ab876164105
Ali Can <can.ali73@gmail.com>
2020-08-31 14:33:28 +0100

    hollo-world-app.py added

--- python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -1,22 +1,10 @@
 from flask import Flask
 
-app =  Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def head():
-    return "Hello World!"
-
-@app.route("/second")
-def second():
-    return "Hello AI ERA!"
-
-@app.route("/third/subthird")
-def third():
-    return "Subthird of the Third!!!"
-
-@app.route("/fourth/<string:id>")
-def fourth(id):
-    return f"Dinamic ID is: {id}"
-
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 80)
+@app.route('/')
+def hello():
+    return 'Hello World'
+    
+if __name__=='__main__':
+   app.run(host='0.0.0.0', port=80)
